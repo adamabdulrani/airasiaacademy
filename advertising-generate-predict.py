f542bbf7589038be2b801866c75b6add674cd780
adamabdulrani <154412390+adamabdulrani@users.noreply.github.com>
2023-12-21 13:27:53 +0800

    Update advertising-generate-predict.py

--- advertising-generate-predict.py
+++ advertising-generate-predict.py
@@ -1,9 +1,15 @@
+import altair as alt
 import streamlit as st
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import seaborn as sns
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from keras.models import load_model
+import pickle
 
 st.write("# Simple Advertising Prediction App") #write title
 st.write("This app predicts the **Sales** type!") #write as subtitle
@@ -11,9 +17,9 @@ st.write("This app predicts the **Sales** type!") #write as subtitle
 st.sidebar.header('User Input Parameters') #to create sidebar
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('TV', 0.70, 296.40, 149.75) #st.slider : element of interaction (name of sidebar, minimum value, maximum value, default) #sepal_length = variable
-    sepal_width = st.sidebar.slider('Radio', 0.00, 36.52, 22.90)
-    petal_length = st.sidebar.slider('Newspaper', 0.30, 45.10, 25.75)
+    TV = st.sidebar.slider('TV', 0.70, 296.40, 149.75) #st.slider : element of interaction (name of sidebar, minimum value, maximum value, default) #sepal_length = variable
+    Radio = st.sidebar.slider('Radio', 0.00, 36.52, 22.90)
+    Newspaper = st.sidebar.slider('Newspaper', 0.30, 45.10, 25.75)
     data = {'TV': TV, 
             'Radio': Radio,
             'Newspaper': Newspaper}
