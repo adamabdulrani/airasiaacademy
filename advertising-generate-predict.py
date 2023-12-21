b834d4c751088f5b931ddcf1d4ad5ef1643c82ec
adamabdulrani <154412390+adamabdulrani@users.noreply.github.com>
2023-12-21 14:32:09 +0800

    Update advertising-generate-predict.py

--- advertising-generate-predict.py
+++ advertising-generate-predict.py
@@ -3,43 +3,51 @@ import pandas as pd
 import pickle
 import os
 
-st.write("# Simple Advertising Prediction App")  # Write title
-st.write("This app predicts the **Sales** type!")  # Write as subtitle
-
-st.sidebar.header('User Input Parameters')  # To create sidebar
-
+# Function to get user input features
 def user_input_features():
-    TV = st.sidebar.slider('TV', 0.70, 296.40, 149.75)  # st.slider: element of interaction (name of sidebar, minimum value, maximum value, default) #sepal_length = variable
+    TV = st.sidebar.slider('TV', 0.70, 296.40, 149.75)
     Radio = st.sidebar.slider('Radio', 0.00, 36.52, 22.90)
     Newspaper = st.sidebar.slider('Newspaper', 0.30, 45.10, 25.75)
-    data = {'TV': TV,
-            'Radio': Radio,
-            'Newspaper': Newspaper}
+    data = {'TV': TV, 'Radio': Radio, 'Newspaper': Newspaper}
     return data
 
-df = user_input_features()
-file_path = "AdvertisingANN.h5"  # Change file extension to .pkl
+# Main Streamlit app
+def main():
+    st.write("# Simple Advertising Prediction App")
+    st.write("This app predicts the **Sales** type!")
 
-try:
-    with open(file_path, "rb") as file:
-        loaded_model = pickle.load(file)
+    st.sidebar.header('User Input Parameters')
 
-    input_data = pd.DataFrame(df, index=[0])
+    # Get user input
+    df = user_input_features()
 
-    input_data['MissingFeature'] = 0  # Assuming this is a placeholder for a missing feature
+    # Model loading and prediction
+    file_path = "AdvertisingANN.pkl"  # Assuming it's a Pickle file
+    try:
+        with open(file_path, "rb") as file:
+            loaded_model = pickle.load(file)
 
-    prediction = loaded_model.predict(input_data.values)
-    st.subheader('User Input parameters')  # Alternate function for subheader
-    st.write(df)
+        # Create DataFrame for input data
+        input_data = pd.DataFrame(df, index=[0])
 
-    st.subheader('Prediction')
-    st.write(prediction)
+        # Prediction
+        prediction = loaded_model.predict(input_data.values)
 
-except FileNotFoundError:
-    st.error(f"Model file {file_path} not found. Please check the file path.")
-except Exception as e:
-    st.error(f"An error occurred: {str(e)}")
+        # Display results
+        st.subheader('User Input parameters')
+        st.write(df)
 
-# Debugging information
-st.write("Current working directory:", os.getcwd())
-st.write("List of files in the directory:", os.listdir())
+        st.subheader('Prediction')
+        st.write(prediction)
+
+    except FileNotFoundError:
+        st.error(f"Model file {file_path} not found. Please check the file path.")
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+
+    # Debugging information
+    st.write("Current working directory:", os.getcwd())
+    st.write("List of files in the directory:", os.listdir())
+
+if __name__ == "__main__":
+    main()
